68eb664f9a459506fd5fe9c9bac92bfbe5fe2fd2
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2025-11-10 06:19:44 +0900

    Add files via upload

---
+++ vs2017/ModbusMaster/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ModbusMaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_PortName = new System.Windows.Forms.ComboBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.button_Read = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox_PortName
+            // 
+            this.comboBox_PortName.FormattingEnabled = true;
+            this.comboBox_PortName.Location = new System.Drawing.Point(12, 12);
+            this.comboBox_PortName.Name = "comboBox_PortName";
+            this.comboBox_PortName.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_PortName.TabIndex = 0;
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(170, 29);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.button_Connect.TabIndex = 1;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // button_Read
+            // 
+            this.button_Read.Location = new System.Drawing.Point(170, 80);
+            this.button_Read.Name = "button_Read";
+            this.button_Read.Size = new System.Drawing.Size(75, 23);
+            this.button_Read.TabIndex = 2;
+            this.button_Read.Text = "Read";
+            this.button_Read.UseVisualStyleBackColor = true;
+            this.button_Read.Click += new System.EventHandler(this.button_Read_Click);
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Location = new System.Drawing.Point(12, 56);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(138, 316);
+            this.textBox_Result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 526);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.button_Read);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.comboBox_PortName);
+            this.Name = "Form1";
+            this.Text = "ModbusMaster";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_PortName;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Button button_Read;
+        private System.Windows.Forms.TextBox textBox_Result;
+    }
+}
+
--- vs2017/ModbusMaster/Form1.cs
+++ vs2017/ModbusMaster/Form1.cs
@@ -1,1 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
 
+using Modbus.Device;
+
+namespace ModbusMaster
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private IModbusSerialMaster master;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSerialPorts();
+        }
+
+        private void LoadSerialPorts()
+        {
+            comboBox_PortName.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            comboBox_PortName.Items.AddRange(ports);
+
+            if (ports.Length > 0)
+                comboBox_PortName.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            string portName = comboBox_PortName.SelectedItem.ToString();
+
+            serialPort = new SerialPort(portName)
+            {
+                BaudRate = 115200,
+                DataBits = 8,
+                Parity = Parity.Even,
+                StopBits = StopBits.One
+            };
+            serialPort.Open();
+
+            master = ModbusSerialMaster.CreateRtu(serialPort);
+
+            MessageBox.Show("Connected to " + portName);
+        }
+
+        private void button_Read_Click(object sender, EventArgs e)
+        {
+            if (master == null)
+            {
+                MessageBox.Show("Not connected.");
+                return;
+            }
+
+            byte slaveId = 2;
+            ushort startAddress = 0;
+            ushort numRegisters = 10;
+
+            ushort[] registers = master.ReadHoldingRegisters(slaveId, startAddress, numRegisters);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < registers.Length; i++)
+            {
+                sb.AppendLine($"Reg[{startAddress + i}] = {registers[i]}");
+            }
+
+            textBox_Result.Text = sb.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+                serialPort.Close();
+        }
+
+    }
+}
